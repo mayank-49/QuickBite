aae887e30a26f317cd2bc76d61aba4eba5deeb19
mayank-49 <mayankkuma04@gmail.com>
2025-03-28 12:43:41 +0530

    updated success bg

--- src/Pages/Success.jsx
+++ src/Pages/Success.jsx
@@ -8,10 +8,10 @@ const Success = () => {
     },3000)
   },[])
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-    {loading ? <PropagateLoader color='#DC2626'/> : <div>
-        <h2 className='text-3xl font-semibold mb-4 text-center text-red-600'>Order Successfull!</h2>
-        <p className='text-lg text-center'>Your order has been succesfully placed</p>
+    <div className='flex flex-col items-center justify-center h-screen bg-red-600'>
+    {loading ? <PropagateLoader color='#ffffff'/> : <div>
+        <h2 className='text-3xl font-semibold mb-4 text-center text-white'>Order Successfull!</h2>
+        <p className='text-lg text-center text-white'>Your order has been succesfully placed</p>
       </div>}
       
     </div>
