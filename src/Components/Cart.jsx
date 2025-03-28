754afcc11eebad7b7ba21594074abcbc6ae69c3d
mayank-49 <mayankkuma04@gmail.com>
2025-03-28 12:39:50 +0530

    updated scrool in cart

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -27,33 +27,36 @@ const Cart = () => {
           <span className="text-xl font-bold text-gray-800">My Order</span>
           <IoMdClose
             onClick={() => setActiveCart(!activeCart)}
-            className="border border-gray-600 text-gray-600 font-bold p-1 text-xl rounded-md hover:text-red-500 hover:border-red-500 cursor-pointer"
+            className="border-2 border-gray-600 text-gray-600 font-bold p-1 text-2xl rounded-md transition-all duration-500 ease-in-out hover:text-red-600 hover:border-red-600 cursor-pointer"
           />
         </div>
-        {cartItems.length > 0 ? cartItems.map((food) => {
-          return (
-            <ItemCard
-              key={food.id}
-              id={food.id}
-              name={food.name}
-              price={food.price}
-              img={food.img}
-              qty={food.qty}
-            />
-          );
-        }): <h2 className="text-center text-xl font-bold text-gray-800 ">Your cart is empty</h2>}
-        {/* <ItemCard/> */}
-        <div className="absolute bottom-0">
+        <div className="overflow-y-auto h-[65vh]">
+          {cartItems.length > 0 ? cartItems.map((food) => {
+            return (
+              <ItemCard
+                key={food.id}
+                id={food.id}
+                name={food.name}
+                price={food.price}
+                img={food.img}
+                qty={food.qty}
+              />
+            );
+          }): <h2 className="text-center text-xl font-bold text-gray-800 ">Your cart is empty</h2>}
+          {/* <ItemCard/> */}
+        </div>
+        <div className="absolute bottom-0 border-t-4 border-red-600">
           <h3 className="font-semibold text-gray-800">Items : {totalQty}</h3>
           <h3 className="font-semibold text-gray-800">Total Amount : {totalPrice}</h3>
           <hr className="w-[90vw] lg:w-[18vw] my-2" />
-          <button onClick={()=>{navigate("/success")}} className="bg-red-500 font-bold px-3 py-2 w-[90vw] lg:w-[18vw] text-white rounded-lg mb-5 cursor-pointer">
+          <button onClick={()=>{navigate("/success")}} className="bg-red-600 hover:bg-red-700 font-bold px-3 py-2 w-[90vw] lg:w-[18vw] text-white rounded-lg mb-5 cursor-pointer">
             Checkout
           </button>
         </div>
       </div>
       <FaCartShopping
-        className={`rounded-full bg-black text-white shadow-md text-5xl p-3 fixed bottom-4 right-4 cursor-pointer ${totalQty>0 && 'animate-bounce delay-500 transition-all '}`}
+        className={`rounded-full bg-black text-white
+           shadow-md text-5xl p-3 fixed bottom-4 right-4 cursor-pointer ${totalQty>0 && 'animate-bounce delay-500 transition-all '}`}
         onClick={() => setActiveCart(!activeCart)}
       />
     </>
--- src/Components/ItemCard.jsx
+++ src/Components/ItemCard.jsx
@@ -1,39 +1,50 @@
 import React from "react";
 import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 import { MdDelete } from "react-icons/md";
-import {removeFromCart} from "../redux/slices/CartSlice"
+import { removeFromCart } from "../redux/slices/CartSlice";
 import { useDispatch } from "react-redux";
-import { incrementQty,decrementQty } from "../redux/slices/CartSlice";
+import { incrementQty, decrementQty } from "../redux/slices/CartSlice";
 import toast, { Toaster } from "react-hot-toast";
 
 const ItemCard = ({ id, name, price, qty, img }) => {
   const dispatch = useDispatch();
-  function handleDelete(){
-    dispatch(removeFromCart({id,name,price,qty,img}));
+
+  function handleDelete() {
+    dispatch(removeFromCart({ id, name, price, qty, img }));
     toast.error(`${name} Removed!`);
   }
-  function handleIncrement(){
-    qty>=1 ? dispatch(incrementQty({id})) : (qty=0)
+
+  function handleIncrement() {
+    qty >= 1 ? dispatch(incrementQty({ id })) : (qty = 0);
   }
-  function handleDecrement(){
-    qty>1 ? dispatch(decrementQty({id})) : (qty=0)
+
+  function handleDecrement() {
+    qty > 1 ? dispatch(decrementQty({ id })) : (qty = 0);
   }
+
   return (
-    <div className="flex gap-2 shadow-md rounded-lg p-2 mb-3">
-      <MdDelete className="absolute right-7 text-gray-600 cursor-pointer" onClick={handleDelete} />
-      <img
-        src={img}
-        alt=""
-        className="w-[50px] h-[50px] "
-      />
+    <div className=" relative flex gap-2 shadow-md rounded-lg p-2 mb-3">
+      <button
+        className="absolute top-2 right-2 text-black hover:text-red-600 cursor-pointer"
+        onClick={handleDelete}
+      >
+        <MdDelete />
+      </button>
+      <img src={img} alt="" className="w-[50px] h-[50px] " />
       <div className="w-full leading-5">
         <h2 className="font-bold text-gray-800">{name}</h2>
         <div className="flex justify-between">
           <span className="text-red-500 font-bold">${price}</span>
           <div className="flex justify-center items-center gap-2">
-            <AiOutlineMinus onClick={handleDecrement} className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-red-500 hover:border-none rounded-md p-1 text-xl transition-all duration-500 ease-linear cursor-pointer" />
+            <AiOutlineMinus
+              onClick={handleDecrement}
+              className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-red-500 hover:border-none rounded-md p-1 text-xl transition-all duration-500 ease-linear cursor-pointer"
+            />
             <span>{qty}</span>
-            <AiOutlinePlus onClick={handleIncrement} className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-red-500 hover:border-none rounded-md p-1 text-xl transition-all duration-500 ease-linear cursor-pointer" />
+            <AiOutlinePlus
+              onClick={handleIncrement}
+              className="border-2 border-gray-600 text-gray-600 hover:text-white hover:bg-red-500 hover:border-none rounded-md p-1 text-xl transition-all duration-500 ease-linear cursor-pointer"
+            />
           </div>
         </div>
       </div>
